d1acfc083a16278b132240e2279d16545ee87c5b
ShahinRiaz2004 <130464629+ShahinRiaz2004@users.noreply.github.com>
2024-05-13 21:30:39 +0430

    Restful API created with node.js

---
+++ server.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+// const express = require('express');
+// // const app = express();
+// // const mongoose = require('mongoose')
+
+// // mongoose.connect('process.env.DATABASE_URL',{useNewUrlParser:true})
+
+// // const db = mongoose.connection
+// // db.on('error', (error) => console.error(error))
+// // db.once('open', () => console.log('Connected to Database'))
+
+// // app.listen(3000, () => console.log("Server Started"))
+// const express = require('express');
+// const mongoose = require('mongoose')
+
+
+// mongoose.connect('mongodb://0.0.0.0:27012/testdb')
+// const db = mongoose.connection
+
+// db.on('error', (err) => {
+//   console.log(err)
+// })
+
+// db.once('open', () => {
+//   console.log('Database connection established')
+// })
+ 
